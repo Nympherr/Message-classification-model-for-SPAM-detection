d24d7ac56e048b160ab8d29220a42c0e85778c70
Lukas St <lukasstauskas@yahoo.com>
2024-12-11 21:39:07 +0200

    NBK model

--- app/main.py
+++ app/main.py
@@ -54,6 +54,31 @@ def algorithm_lrk(message):
 
     return prediction[0]
 
+# ----- NKB modelis ------
+model_nkb = joblib.load('./trained_models/NBK/model.pkl')
+vectorizer_nkb = joblib.load('./trained_models/NBK/vectorizer.pkl')
+
+def algorithm_nkb(message):
+
+    message_length = len(message)
+    message_punct_count = len(re.findall(punctuation_pattern, message))
+    message_vector = vectorizer_avk.transform([message])
+
+    word_count = len(message.split())
+    number_count = len(re.findall(r'\d+', message))
+    standalone_number_count = len(re.findall(r'\b\d+\b', message))
+    average_word_length = sum(len(word) for word in message.split()) / word_count if word_count > 0 else 0
+    ratio_words_punctuation = word_count / message_punct_count if message_punct_count > 0 else 0
+
+    vectorized_message_values = sp.hstack([
+        message_vector,
+        sp.csr_matrix([[message_length, message_punct_count, word_count, number_count, standalone_number_count, average_word_length, ratio_words_punctuation]])
+    ])
+
+    result = model_avk.predict(vectorized_message_values)
+
+    return result[0]
+
 app = Flask(__name__)
 
 history = []
@@ -74,7 +99,7 @@ def classify():
     elif algorithm == 'LRK':
         result = algorithm_lrk(message)
     elif algorithm == 'NBK':
-        result = 'neimplementuotas dar'
+        result = algorithm_nkb(message)
     elif algorithm == 'k-nearest':
         result = 'neimplementuotas dar'
     else:
--- app/model_training_scripts/NKB.py
+++ app/model_training_scripts/NKB.py
@@ -1,17 +1,18 @@
-# ----- Atraminių vektorių klasifikatorius -----
+# ----- Naive Bayes klasifikatorius -----
 
-from joblib import dump
+from sklearn import metrics
 import scipy.sparse as sp
-from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 import pandas as pd
+from joblib import dump
 
 df = pd.read_csv("./../datasets/dataset_unbalanced.csv", encoding="latin1")
 
 X = df[['message', 'length', 'punct_count', 'word_count', 'number_count', 'standalone_number_count', 'average_word_length', 'ratio_words_punctuation']]
 Y = df['value']
+
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
 
 vectorizer = TfidfVectorizer()
@@ -21,13 +22,13 @@ X_test_message = vectorizer.transform(X_test['message'])
 X_train_combined = sp.hstack([X_train_message, sp.csr_matrix(X_train[['length', 'punct_count', 'word_count', 'number_count', 'standalone_number_count', 'average_word_length', 'ratio_words_punctuation']].values)])
 X_test_combined = sp.hstack([X_test_message, sp.csr_matrix(X_test[['length', 'punct_count', 'word_count', 'number_count', 'standalone_number_count', 'average_word_length', 'ratio_words_punctuation']].values)])
 
-svc_model = SVC(gamma="scale")
-svc_model.fit(X_train_combined, Y_train)
+nb_model = MultinomialNB()
+nb_model.fit(X_train_combined, Y_train)
 
-dump(svc_model, './../trained_models/AVK/model.pkl')
-dump(vectorizer, './../trained_models/AVK/vectorizer.pkl')
-
-predictions = svc_model.predict(X_test_combined)
+predictions = nb_model.predict(X_test_combined)
 accuracy = metrics.accuracy_score(Y_test, predictions)
 print(f"Atlikta. Modelio Tikslumas: {accuracy * 100:.2f}%")
 print(metrics.classification_report(Y_test, predictions))
+
+dump(nb_model, './../trained_models/NBK/model.pkl')
+dump(vectorizer, './../trained_models/NBK/vectorizer.pkl')
